849c2b5c57e0d1b788c0814c222eb0fa2f4b5471
Tarutyyyne <s1f102500405@iniad.org>
2025-05-10 14:18:15 +0900

    'userInputsとbombMapを渡すと一度だけ爆弾をセットする関数の外側が完成'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,17 @@
 
 import { useState } from 'react';
 import styles from './page.module.css';
-
+//初めてのクリックで爆弾を配置する関数
+const setMine = (userInputs: number[][], bombMap: number[][]): number[][] => {
+  //userInputsの要素が全て0ならばそれは初回と同義
+  if (userInputs.flat().filter((userInputs) => userInputs === 1).length === 0) {
+    console.log('check setMine');
+    // console.log('ここにランダムに1を入れる')
+    // console.log('そして得られた配列を如何に投げる')
+    bombMap[0][0] = 1;
+  }
+  return bombMap;
+};
 export default function Home() {
   const [userInputs, setUserInputs] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -30,21 +40,25 @@ export default function Home() {
   ]);
 
   const clickCell = (x: number, y: number, newUserInputs: number[][]) => {
-    console.log('current coordinate', y, x);
+    setBombMap(setMine(userInputs, bombMap));
     newUserInputs[y][x] = 1;
+    setUserInputs(newUserInputs);
+    console.log('click');
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.userInputs}>
-        {userInputs.map((row, y) =>
+        {bombMap.map((row, y) =>
           row.map((column, x) => (
             <div
               className={styles.cell}
               style={{ backgroundPosition: '-420px' }}
               key={`${x}-${y}`}
               onClick={() => clickCell(y, x, newUserInputs)}
-            />
+            >
+              {column === 1 ? 1 : 0}
+            </div>
           )),
         )}
       </div>
