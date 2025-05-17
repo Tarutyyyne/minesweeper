5a2549dab09d871c9c742d624f6acd7b6161156c
Tarutyyyne <s1f102500405@iniad.org>
2025-05-18 01:35:54 +0900

    '迷走中　とりあえずcss許さない'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,24 +29,23 @@ const directions: number[][] = [
   [0, -1],
   [-1, -1],
 ];
-
-//x, y座標を一つの数字にする関数
-const makeIndex = (y: number, x: number): number => {
+const makeIndex = (y: number, x: number) => {
   return y * HEIGHT + x;
 };
-//newBombMapをいじるだけ
-const makeMineMap = (
-  userInputs: number[][],
-  mineMap: number[][],
-  newMineMap: number[][],
+
+const makeBombMap = (
   clickX: number,
   clickY: number,
+  userInputs: number[][],
+  bombMap: number[][],
+  newBombMap: number[][],
+  directions: number[][],
 ) => {
-  const countInputs: number = userInputs.flat().filter((userInputs) => userInputs === OPEN).length;
+  const countInputs: number = userInputs.flat().filter((userInput) => userInput === OPEN).length;
   if (userInputs[clickY][clickX] < OPEN && countInputs === 0) {
-    const deleteXY: number = makeIndex(clickX, clickY);
+    const deleteXY: number = makeIndex(clickY, clickX);
     const randomIndexArray: number[] = Array.from(
-      { length: mineMap.flat().length },
+      { length: bombMap.flat().length },
       (_, index) => index,
     ).filter((index) => index !== deleteXY);
     const copyRandomIndexArray: number[] = [];
@@ -57,28 +56,26 @@ const makeMineMap = (
     }
     const randomIndex: number[] = copyRandomIndexArray.splice(0, HOW_MANY_BOMB);
     for (let j = 0; j < HOW_MANY_BOMB; j++) {
-      const randomX = Math.floor(randomIndex[j] / WIDTH);
-      const randomY = randomIndex[j] % HEIGHT;
-      newMineMap[randomY][randomX] = BOMB;
-      // for (let k = 0; k < 8; k++) {
-      //   const predictX = randomX + directions[k][1];
-      //   const predictY = randomY + directions[k][0];
-      //   //undefinedが返されたらこのif文でcontinueさせる
-      //   if (bombMap[predictY] === undefined) {
-      //     continue;
-      //   }
-      //   //上のおかげで「もしundefinedじゃないなら」というifのネストを減らせる
-      //   if (bombMap[predictY][predictX] === BOMB && newBombMap[predictY][predictX] === BOMB) {
-      //     continue;
-      //   } else if (
-      //     bombMap[predictY][predictX] !== BOMB &&
-      //     newBombMap[predictY][predictX] !== BOMB
-      //   ) {
-      //     newBombMap[predictY][predictX] += SAFE[1];
-      //   } else {
-      //     continue;
-      //   }
-      // }
+      const randomX: number = Math.floor(randomIndex[j] / WIDTH);
+      const randomY: number = randomIndex[j] % HEIGHT;
+      newBombMap[randomY][randomX] = BOMB;
+      for (let k = 0; k < 8; k++) {
+        const predictX: number = randomX + directions[k][1];
+        const predictY: number = randomY + directions[k][0];
+        if (bombMap[predictY] === undefined) {
+          continue;
+        }
+        if (bombMap[predictY][predictX] === BOMB && newBombMap[predictY][predictX] === BOMB) {
+          continue;
+        } else if (
+          bombMap[predictY][predictX] !== BOMB &&
+          newBombMap[predictY][predictX] !== BOMB
+        ) {
+          console.log('predict');
+        } else {
+          continue;
+        }
+      }
     }
   }
 };
@@ -86,69 +83,20 @@ const makeMineMap = (
 export default function Home() {
   const [userInputs, setUserInputs] = useState(copyDefaultBoard);
   const newUserInputs: number[][] = structuredClone(userInputs);
-  const [mineMap, setMineMap] = useState(copyDefaultBoard);
-  const newMineMap: number[][] = structuredClone(mineMap);
-  // もろもろの処理はこの中に書こうかな
-  const gameBoard: number[][] = defaultBoard;
-  const makeGameBoard = (userInputs: number[][], mineMap: number[][]): number[][] => {
-    for (let i = 0; i < HEIGHT; i++) {
-      for (let j = 0; j < WIDTH; j++) {
-        gameBoard[j][i] = mineMap[i][j] + userInputs[i][j];
-      }
-    }
-    //予測値を書く
-    const copyGameBoard: number[] = structuredClone(mineMap).flat();
-    const mineCoordinateArray: number[] = [];
-    for (let i = 0; i < WIDTH * HEIGHT; i++) {
-      if (copyGameBoard[i] !== BOMB) {
-        continue;
-      } else {
-        mineCoordinateArray.push(i);
-      }
-    }
-    console.log(mineCoordinateArray);
-    for (let j = 0; j < HOW_MANY_BOMB; j++) {
-      const mineX: number = Math.floor(mineCoordinateArray[j] / WIDTH);
-      const mineY: number = mineCoordinateArray[j] % HEIGHT;
-      for (let k = 0; k < 8; k++) {
-        const predictX = mineX + directions[k][1];
-        const predictY = mineY + directions[k][0];
-        //undefinedが返されたらこのif文でcontinueさせる
-        if (
-          mineMap[predictY] === undefined ||
-          gameBoard[predictY] === undefined ||
-          mineMap[predictY][predictX] === undefined ||
-          gameBoard[predictY][predictX] === undefined
-        ) {
-          continue;
-        }
-        //上のおかげで「もしundefinedじゃないなら」というifのネストを減らせる
-        if (mineMap[predictY][predictX] === BOMB) {
-          continue;
-        } else if (mineMap[predictY][predictX] !== BOMB) {
-          gameBoard[predictX][predictY] += 1;
-        } else {
-          continue;
-        }
-      }
-    }
-    return gameBoard;
-  };
+  const [bombMap, setBombMap] = useState(copyDefaultBoard);
+  const newBombMap: number[][] = structuredClone(bombMap);
 
   const clickCell = (
     clickX: number,
     clickY: number,
     userInputs: number[][],
     newUserInputs: number[][],
-    mineMap: number[][],
-    newMineMap: number[][],
-    directions: number[][],
+    bombMap: number[][],
+    newBombMap: number[][],
   ) => {
-    makeMineMap(userInputs, mineMap, newMineMap, clickX, clickY);
-    setMineMap(newMineMap);
-    // makeGameboardをぶちこむ
-    makeGameBoard(userInputs, mineMap);
-    newUserInputs[clickX][clickY] = OPEN;
+    makeBombMap(clickX, clickY, userInputs, bombMap, newBombMap, directions);
+    setBombMap(newBombMap);
+    newUserInputs[clickY][clickX] = OPEN;
     setUserInputs(newUserInputs);
   };
 
@@ -158,14 +106,12 @@ export default function Home() {
         className={styles.userInputs}
         style={{ width: `${30 * WIDTH}px`, height: `${30 * HEIGHT}px` }}
       >
-        {makeGameBoard(newUserInputs, newMineMap).map((row, y) =>
+        {bombMap.map((row, y) =>
           row.map((column, x) => (
             <button
               className={styles.cell}
               key={`${x}-${y}`}
-              onClick={() =>
-                clickCell(x, y, userInputs, newUserInputs, mineMap, newMineMap, directions)
-              }
+              onClick={() => clickCell(x, y, userInputs, newUserInputs, bombMap, newBombMap)}
             >
               {column}
             </button>
